01d4b2cfee4e1133d559493671815c7250d3def7
JBalajiReddy <jeedipallybalajireddy@gmail.com>
2025-05-22 22:43:53 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,332 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Shortnest",
+    description:
+      "Smart and secure URL shortener with analytics, JWT auth, and full-stack deployment.",
+    subDescription: [
+      "Shortnest streamlines the process of URL shortening, making sharing links effortless and efficient. With its user-friendly interface, Shortnest allows you to generate concise, easy-to-share URLs in seconds. Simplify your sharing experience with Shortnest today.",
+      "Implemented secure authentication and database management using JWT authentication and PostgresSQL.",
+      "Designed a responsive frontend with React, Tailwind CSS, enhancing user experience.",
+      "Added a dashboard section to analyze the shortened url's.",
+    ],
+    href: "https://shortnest-jbr.netlify.app/",
+    logo: "",
+    image: "/assets/projects/Shortnest.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Spring",
+        path: "/assets/logos/Spring.svg",
+      },
+      {
+        id: 3,
+        name: "Docker",
+        path: "/assets/logos/Docker.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "NeuraReply - AI-Powered Email Reply Assistant",
+    description:
+      "NeuraReply is a Chrome extension to boost your email productivity!",
+    subDescription: [
+      "🚀 AI-Powered Replies: Generates professional email responses instantly.",
+      "🎨 Modern UI: Built with React for a smooth user experience.",
+      "🔗 Seamless Gmail Integration: Works directly within Gmail's compose window.",
+      "🛠 Spring Boot Backend: Handles AI processing and API requests efficiently.",
+      "☁ Google Gemini API: Ensures high-quality AI-generated replies.",
+    ],
+    href: "https://github.com/JBalajiReddy/Neural-4orce",
+    logo: "",
+    image: "/assets/projects/neura-reply.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Spring",
+        path: "/assets/logos/Spring.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 4,
+        name: "Gemini",
+        path: "/assets/logos/gemini.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Personal Portfolio(This Website)",
+    description:
+      "A personal portfolio website showcasing my projects and skills.",
+    subDescription: [
+      "Developed a fully interactive Single Page portfolio website using React and EmailJs.",
+      "Contains an interactive About section with a 3D rotating globe.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+      "Integrated EmailJS for seamless contact form submissions.",
+      "Implemented smooth scrolling and animations for a polished user experience.",
+    ],
+    href: "https://jbalajireddy.netlify.app/",
+    logo: "",
+    image: "/assets/projects/portfolio.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 4,
+        name: "Vite",
+        path: "/assets/logos/vitejs.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Social Media App(Under Development)",
+    description:
+      "A social media application with user profiles, posts, and comments.",
+    subDescription: [
+      "Under development, this project aims to create a social media platform with features like user profiles, posts, and comments.",
+    ],
+    href: "",
+    logo: "",
+    image: "public/assets/projects/9u2px25j.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/logos/nextjs.png",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+      // {
+      //   id: 4,
+      //   name: "Microsoft",
+      //   path: "/assets/logos/microsoft.svg",
+      // },
+    ],
+  },
+  {
+    id: 5,
+    title: "AI SaaS App(Under Development)",
+    description:
+      "Build an AI image SaaS platform that excels in image processing capabilities, integrates a secure payment infrastructure, offers advanced image search functionalities, and supports multiple AI features",
+    subDescription: [
+      "👉 Secure Authentication & Authorization: User registration, login, route protection, and profile access with credit details.",
+      "👉 AI-Powered Image Tools: Restore, recolor, fill missing parts, remove objects or backgrounds, and download transformed images.",
+      "👉 Smart Image Management: View transformation details, manage updates/deletions, and showcase community images with pagination.",
+      "👉 Advanced Search: Quickly find images based on content or detected objects.",
+      "👉 Credit System Integration: Earn or purchase credits via Stripe for image transformations.",
+      "👉 Responsive Design: Delivers a seamless, intuitive experience across all device sizes.",
+    ],
+
+    href: "",
+    logo: "",
+    image: "public/assets/projects/06mf1wy6.png",
+    tags: [
+      {
+        id: 1,
+        name: "NextJs",
+        path: "/assets/logos/nextjs.png",
+      },
+      {
+        id: 2,
+        name: "Typescript",
+        path: "public/assets/logos/TypeScript.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "public/assets/logos/MongoDB.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "AI-generated Video Sharing App(Under Development)",
+    description:
+      "A dynamic video-sharing mobile app built with React Native, featuring smooth navigation, media uploads, and real-time search for a seamless social experience.",
+    subDescription: [
+      "👉 Smooth onboarding with intuitive tab navigation across Home, Search, and Profile.",
+      "👉 Secure email auth and user profiles with video uploads and follower insights.",
+      "👉 Animated home feed with pull-to-refresh for an interactive media experience.",
+      "👉 Real-time video search and seamless media uploads via post creation screen.",
+      "👉 Responsive UI with rich animations using Nativewind and Animatable.",
+    ],
+    href: "",
+    logo: "",
+    image: "",
+    tags: [
+      {
+        id: 1,
+        name: "React Native",
+        path: "public/assets/logos/reactnative.png",
+      },
+      {
+        id: 2,
+        name: "Appwrite",
+        path: "public/assets/logos/Appwrite.svg",
+      },
+      {
+        id: 3,
+        name: "Nativewind",
+        path: "public/assets/logos/llmrg0mo.png",
+      },
+      // {
+      //   id: 4,
+      //   name: "TailwindCSS",
+      //   path: "/assets/logos/tailwindcss.svg",
+      // },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "GitHub",
+    href: "https://github.com/JBalajiReddy",
+    icon: "/assets/socials/github.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/jeedipalli-balaji-reddy/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/balajireddyj/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Design Team Member",
+    job: "GeeksforGeeks AUSC (University Student Chapter)",
+    date: "2024-Present",
+    contents: [
+      "Created engaging posters and social media content using Canva to promote events, workshops, and technical sessions...",
+      "Skills: Canva · Figma (Software) · HTML5 · Cascading Style Sheets (CSS) · Teamwork",
+      "Collaborated with the design team to create visually appealing graphics and layouts for various projects.",
+      "Participated in brainstorming sessions to generate creative ideas and concepts for design projects.",
+    ],
+  },
+  {
+    title: "Data Research Analyst",
+    job: "Internship",
+    date: "08/2024-09/2024",
+    contents: [
+      "Worked as a Data Research Analyst on a project for Concentrix Daksh Services from August 4, 2024, to September 22, 2024. ",
+      " conducted in-depth data analysis and research, delivering valuable insights that supported key project decisions.",
+      "This role allowed me to refine my analytical skills and deepen my knowledge in data research methodologies, contributing to actionable outcomes.",
+      "Skills: Analytical Skills · Financial Analysis · Business Analysis · Market Analysis · Teamwork",
+    ],
+  },
+  {
+    title: "Software Development Engineer",
+    job: "Internship",
+    date: "11/2024-12/2024",
+    contents: [
+      "Worked on a project to develop a web application using React and Django, focusing on front-end development and REST API integration.",
+      "Collaborated with a team of developers to implement features and resolve bugs.",
+      "Participated in code reviews and contributed to improving code quality.",
+      "Skills: React.js · Django · PostgreSQL · Python (Programming Language) · JavaScript · Tailwind CSS · HTML5 · Cascading Style Sheets (CSS) · Git",
+    ],
+  },
+  
+];
+export const reviews = [
+  {
+    name: "Shashikanth",
+    username: "@Shashi",
+    body: "I love working with Balaji. He is a great developer and a good friend.",
+    img: "https://robohash.org/shashi",
+  },
+  {
+    name: "Vignesh",
+    username: "@vignesh",
+    body: "He is a hardworking and reliable person.",
+    img: "https://robohash.org/vignesh",
+  },
+  {
+    name: "Hari",
+    username: "@hariv",
+    body: "He is my bro! He is a great developer and a good friend.",
+    img: "https://robohash.org/hari",
+  },
+  {
+    name: "Rithvik",
+    username: "@rithvik",
+    body: "He always helps me with my work.",
+    img: "https://robohash.org/rithvik",
+  },
+  {
+    name: "Yogesh",
+    username: "@yogesh-gfg",
+    body: "TO BE FILLED",
+    img: "https://robohash.org/yogesh",
+  },
+  {
+    name: "Tejaswini",
+    username: "@tejaswini",
+    body: "TO BE FILLED",
+    img: "https://robohash.org/tejaswini",
+  },
+  {
+    name: "Tejasree",
+    username: "@tejasree",
+    body: "He is incredibly talented  and always a pleasure to work with",
+    img: "https://robohash.org/tejasree",
+  },
+  {
+    name: "Gaurav",
+    username: "@gaurav-gfg",
+    body: "TO BE FILLED",
+    img: "https://robohash.org/gaurav",
+  },
+];
